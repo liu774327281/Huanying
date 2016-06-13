b7c2de671a0ea97adb55062dea6c55af5de4117e
liujie774327281 <774327281@qq.com>
2016-06-13 19:10:12 +0800

    地图

---
+++ app/src/main/java/qianfeng/com/huanying/Main2Activity.java
@@ -0,0 +1,31 @@
+package qianfeng.com.huanying;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+    }
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            moveTaskToBack(false);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void di(View view ){
+        Intent intent=new Intent(this,Main3Activity.class);
+        startActivity(intent);
+        finish();
+    }
+}
---
+++ app/src/main/java/qianfeng/com/huanying/MainActivity.java
@@ -0,0 +1,36 @@
+package qianfeng.com.huanying;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private boolean isFirst = true;  //是否是第一次
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+           new Handler().postDelayed(new Runnable() {
+               @Override
+               public void run() {
+                   Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+                   startActivity(intent);
+                   finish();
+               }
+           },3000);
+    }
+
+
+
+
+
+}
+
+
+
